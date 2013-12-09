a5c6c2c69147564d46882132f0d3a37977a4aad6
Jake Ruesch <jsruesch@gmail.com>
2013-12-09 14:36:33 -0600

    Remove styling on new cs page

--- project_citizen_science_bp.php
+++ project_citizen_science_bp.php
@@ -21,22 +21,7 @@
 </head>
 
 <body id="game_page">
-    
-    
-<?php require "navigation.php";?>
 
-
-<div class="banner_project_cs">
-    <div class="banner_text_container">
-
-    </div>
-</div>
-
-<div class="body_content_container">
-    
-    <div class="body_content">
-        
-        <div class="unity_embed_container_cs">
             <OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
                 codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
                 WIDTH="800"
@@ -59,42 +44,6 @@
                         
                 </EMBED>
         </OBJECT>
-        </div>
-        
-        <!-- Citizen Science -->
-        <div class="project_container">
-        
-            <div class="project_image">
-                <div class="project_citizen_science"></div>
-            </div>
-            
-            <div class="project_description">
-                
-                <div class="project_description_header">
-                    <p>Citizen Science</p>
-                </div>
-                
-                <div class="project_description_paragraph">
-                    <p>
-                        Citizen Science is an online flash-based computer adventure game in which the
-                        player is a young adult who becomes concerned about the health of a local lake
-                        threatened by eutrophication. Based at Lake Mendota in Madison, WI, the player's
-                        goal is to restore the lake. By focusing on the ecological needs of Lake Mendota
-                        as well as the surrounding community, the game is able to bring together real-world
-                        issues and scientific practices.
-                    </p>
-                </div>
-                
-            </div>
-            
-        </div>
-        
-        
-    </div>
-    
-</div>
-
-<?php require "footer.php";?>
 
  
 </body>
