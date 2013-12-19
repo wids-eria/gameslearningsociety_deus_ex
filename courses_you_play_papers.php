5e1ecd7c8c3179fac3b853c23c74ec8eada4a934
Jake Ruesch <jsruesch@gmail.com>
2013-12-19 14:00:41 -0600

    Add You Play Pages

---
+++ courses_you_play_papers.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML >
+<html lang="en">
+<head>
+    
+    <title>Games Learning Society</title>
+    <meta name="description"
+          content="">
+    
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    
+    <!-- Typekit -->
+    <script type="text/javascript" src="//use.typekit.net/mpl0sio.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    
+</head>
+
+<body id="courses_page">
+    
+    
+    
+<?php require "navigation.php";?>
+
+
+<div class="banner_image_courses">
+    <div class="banner_text_container">
+    </div>
+</div>
+
+<div class="body_content_container">
+    
+    <div class="body_content">
+        
+        <div class="full_width floatright topmargin">
+            
+            <p class="subhead_type floatleft">You Play Papers</p>
+            
+            <div class="hp_courses_containter floatleft">
+                
+                
+                    <div class="hp_course_link floatleft">
+                        
+                        <div class="you_play_picture">
+                                <img src="you_play_files/pics/you_play_kyrie.png"/>
+                        </div>
+                        
+                        <div class="you_play_title">
+                            <br />
+                            <p class="course_title">“You Play”: A Study of the Learning System in Half-Life 2</p>
+                            <p class="course_instructor">Kyrie Eleison H. Caldwell</p>
+                            <a href="you_play_files/pdfs/Caldwell_You_Play_Revision.pdf" target="_blank">
+                                <p class="course_pdf">Dowload the PDF</p>
+                            </a>
+                        </div>
+
+                    </div>
+                
+            </div>
+            
+        </div>        
+        
+    </div>
+    
+</div>
+
+
+<?php require "footer.php";?>
+ 
+</body>
+
+
+
+</html>
