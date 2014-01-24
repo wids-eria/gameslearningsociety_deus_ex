8e4d20f27498be8a41cd03547872651263f3c4bd
asalmon <allisonmariesalmon@gmail.com>
2014-01-24 10:12:40 -0600

    try a different way of embedding

--- project_citizen_science_bp.php
+++ project_citizen_science_bp.php
@@ -50,29 +50,12 @@
 
 <body id="game_page">
 
-            <OBJECT classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000"
-                codebase="http://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=6,0,0,0"
-                WIDTH="800"
-                HEIGHT="600"
-                MARGIN-LEFT="50"
-                id="game" name="game" ALIGN="">
-
-                <PARAM NAME=movie
-                VALUE="Main">
-                <PARAM NAME=quality VALUE=high>
-                <PARAM NAME=bgcolor VALUE=#000000>
-                
-                <EMBED src="cs_build/Main.swf"
-                quality=high bgcolor=#000000
-                WIDTH="881" HEIGHT="600"
-                NAME="Main"
-                ALIGN=""
-                TYPE="application/x-shockwave-flash"
-                PLUGINSPAGE="http://www.macromedia.com/go/getflashplayer">
-                        
-                </EMBED>
-        </OBJECT>
-
+        <object type="application/x-shockwave-flash" id="game" name="game"
+         align="middle" data="cs_build/Main.swf" width="881" height="600"><param name="wmode" value="opaque">
+        <param name="quality" value="high"><param name="bgcolor" value="#ffffff">
+        <param name="allowscriptaccess" value="always"><param name="allowfullscreen" value="true">
+        <param name="flashvars" value="brainPop=advanced">
+        </object>
  
 </body>
 
