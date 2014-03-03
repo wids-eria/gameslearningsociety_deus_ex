ff43976713b2903c0581aaf67fb7ebe717e37c75
Jake Ruesch <jsruesch@gmail.com>
2014-03-03 13:23:54 -0600

    Add conference page that redirects to glsc site

---
+++ conference.php
@@ -0,0 +1,30 @@
+<!DOCTYPE HTML >
+<html lang="en">
+<head>
+    
+    <title>Games Learning Society</title>
+    <meta name="description"
+          content="">
+    
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    
+    <!-- Redirect -->
+    <meta http-equiv="refresh" content="1;url=http://glsconference.org/2014/">
+    <script type="text/javascript">
+        window.location.href = "http://glsconference.org/2014/"
+    </script>
+    
+    <!-- Typekit -->
+    <script type="text/javascript" src="//use.typekit.net/mpl0sio.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    
+</head>
+
+<body id="">
+
+</body>
+
+
+
+</html>
